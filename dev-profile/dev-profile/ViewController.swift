3a0006612f9d07b24f4ecb7b0622258f55ab9598
Ryan Egan <ryanegan@Ryans-Mac-Pro.local>
2017-12-08 19:58:52 -0800

    lesson 01 complete

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devslopesProfileLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.'
+        
+        devslopesProfileLogo.layer.cornerRadius = 10
+        devslopesProfileLogo.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
